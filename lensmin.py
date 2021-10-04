d306ddcb46d9acd4f2d961b320206c0a0b7547a7
Tyler Harvey <tyler.r.harvey@gmail.com>
2021-10-04 17:58:45 -0600

    turned on smoothness check in lensmin

--- lensmin.py
+++ lensmin.py
@@ -40,5 +40,5 @@ calc_properties_optics(col)
 col.read_optical_properties()
 
 initial_simplex = None if simplex_filename == 'None' else np.load(simplex_filename)
-optimize_many_shapes(obj,col,obj.mag_mat_z_indices+obj.coil_z_indices,obj.mag_mat_r_indices+obj.coil_r_indices,z_min=None,z_max=z_max,r_min=r_min,r_max=None,simplex_scale=simplex_scale,options={'disp':True,'xatol':0.01,'fatol':0.001,'adaptive':True,'initial_simplex':initial_simplex,'return_all':True}) # ,'maxfev':100000 #,method='Nelder-Mead',manual_bounds=True,options={'disp':True,'xatol':0.01,'fatol':0.001,'adaptive':True,'initial_simplex':None})
+optimize_many_shapes(obj,col,obj.mag_mat_z_indices+obj.coil_z_indices,obj.mag_mat_r_indices+obj.coil_r_indices,z_min=None,z_max=z_max,r_min=r_min,r_max=None,enforce_smoothness=True,simplex_scale=simplex_scale,options={'disp':True,'xatol':0.01,'fatol':0.001,'adaptive':True,'initial_simplex':initial_simplex,'return_all':True}) # ,'maxfev':100000 #,method='Nelder-Mead',manual_bounds=True,options={'disp':True,'xatol':0.01,'fatol':0.001,'adaptive':True,'initial_simplex':None})
 
